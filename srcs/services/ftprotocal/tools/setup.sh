f0b0b85b964ecbd07072ec7ef64014344c4bee80
knakto <kasichonooo@gmail.com>
2025-09-22 03:04:55 +0700

    add:ftprotocal server to put file in wordpress

---
+++ srcs/services/ftprotocal/tools/setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+useradd -m ftpuser # -m for make new home
+echo "ftpuser:idontknow" | chpasswd
+
+# chmod -R 775 /home/ftpuser/wordpress # 775 for ftpuser in www-data group
+groupadd -g 5000 wp_users
+usermod -aG wp_users ftpuser
+
+# new ftp path need to use
+mkdir -p /home/ftpuser/empty
+chown -R ftpuser:ftpuser /home/ftpuser/empty
+
+su - ftpuser # "-" for refresh shell env
+ 
+vsftpd /vsftpd.conf
