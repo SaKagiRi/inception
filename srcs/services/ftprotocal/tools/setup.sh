d1b1f7aac8a43850e9719c0c32493887d741d243
knakto <kasichonooo@gmail.com>
2025-09-22 05:06:57 +0700

    fixed: change ftp text password to cretencial file

--- srcs/services/ftprotocal/tools/setup.sh
+++ srcs/services/ftprotocal/tools/setup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+FTP_PASSWD=$(cat /var/run/secrets/FTP_PASSWD)
+
 useradd -m ftpuser # -m for make new home
-echo "ftpuser:idontknow" | chpasswd
+echo "ftpuser:$FTP_PASSWD" | chpasswd
 
 # chmod -R 775 /home/ftpuser/wordpress # 775 for ftpuser in www-data group
 groupadd -g 5000 wp_users
